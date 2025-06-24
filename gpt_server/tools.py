77dbc261db87519497851de03b529d1a7d98d8b9
Sara Maenza <sara.maenza98@gmail.com>
2025-06-24 17:10:15 +0200

    fix bug id automations

--- gpt_server/tools.py
+++ gpt_server/tools.py
@@ -83,7 +83,25 @@ def generate_automation(
     try:
         if automation_id == '-1':
             automations = _db.get_automations(user_id)
-            automation_id = len(automations)+1
+            #automation_id = len(automations)+1
+            if len(automations) == 0:
+                automation_id = 1
+            else:
+                # Trova il massimo ID esistente
+                max_id = 0
+                for automation in automations:
+                    # Gestisci diversi tipi di ID
+                    if isinstance(automation['id'], (int, float)):
+                        # Se è un numero (int o float), usa direttamente
+                        current_id = int(automation['id'])
+                    elif isinstance(automation['id'], str):
+                        # Se è una stringa, prova a convertirla
+                        if automation['id'].replace('.', '').isdigit():
+                            # Gestisce sia "123" che "123.0"
+                            current_id = int(float(automation['id']))
+                    
+                    max_id = max(max_id, current_id)
+                automation_id = max_id + 1
             #automation_id = '1' if len(automations) == 0 else str(int(automations[-1]['id']) + 1)
             data['automation']['id'] = automation_id
         else:
